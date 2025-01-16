477117183c3cc7636ce2f4549ede663fcd99fe36
cerqueirabruno <bocerqueira@hotmail.com>
2025-01-16 10:00:42 -0300

    implementação das rotas de GET /produto:id e POST para cadastro de um novo produto

--- app/backend/src/db/productsDB.js
+++ app/backend/src/db/productsDB.js
@@ -4,6 +4,18 @@ const getAll = () => connection.execute(
   `SELECT * FROM products`,
 );
 
+const getById = (id) => connection.execute(
+  `SELECT * FROM products WHERE id = ?`, [id]
+);
+
+const insert = (product) => connection.execute(
+  `INSERT INTO products 
+    (name, value, description, stock) VALUES (?, ?, ?, ?)`,
+  [product.name, product.value, product.description, product.stock],
+);
+
 module.exports = {
   getAll,
+  getById,
+  insert
 };
--- app/backend/src/routes/productsRouter.js
+++ app/backend/src/routes/productsRouter.js
@@ -1,17 +1,46 @@
 const express = require('express');
 const connection = require('../db/connection');
-const { getAll } = require('../db/productsDB');
+const { getAll, getById, insert } = require('../db/productsDB');
 
 const router = express.Router();
 
 router.get('/', async (request, response) => {
   try {
     const [result] = await getAll();
-    console.log(result);    
     response.status(200).json(result);
   } catch (error) {
     console.log(error.message);
     response.status(500).json({ message: error.sqlMessage });
+  }
+});
+
+router.get('/:id', async (request, response) => {
+  try {
+    const { id } = request.params;
+    const [[result]] = await getById(id);
+
+    if (result) {
+      response.status(200).json(result);
+    } else {
+      response.status(404).json({ message: 'Produto não encontrada' })
+    }
+
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).json({ message: error.sqlMessage });
+  }
+});
+
+router.post('/', async (request, response) => {
+  const product = request.body;
+  
+  try {
+    const [result] = await insert(product);
+    response.status(201).json({
+      message: `Produto cadastrado com sucesso com o id ${result.insertId}` });
+  } catch (error) {
+    console.log(error);
+    response.status(500).json({ message: 'Ocorreu um erro ao cadastrar uma pessoa' });
   }
 });
 
